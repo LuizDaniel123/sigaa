b1e9c9c9b3803673bac444c5bd45334577d0e05e
LuizDaniel123 <luizespindola275@gmail.com>
2023-01-09 04:25:49 -0500

    feat: Adicionei o componente principal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header';
+import Principal from './components/principal';
 
 function App() {
   return (
     <>
     <Header />
+    <Principal />
     </>
   );
 }
