c69b4925c53fd6d06446bec5943e212f3755c1b0
LuizDaniel123 <luizespindola275@gmail.com>
2023-01-09 23:54:40 -0500

    style: Alterei e adicionei estilo para as informações do componente principal

--- src/components/principal/styled.js
+++ src/components/principal/styled.js
@@ -7,7 +7,7 @@ const Container = styled.div`
 
     div#quatro-divs {
         display: flex;
-        height: 15%;
+        height: 8%;
         width: 100%;
         justify-content: space-between;
 
@@ -53,8 +53,7 @@ const Container = styled.div`
         align-items: center;
         display: flex;
         flex-direction: column;
-        justify-content: center;
-        height: 30%;
+        height: 20%;
         width: 100%;
 
         h1 {
@@ -69,18 +68,14 @@ const Container = styled.div`
             border: 3px solid #C8D5EC;
             display: flex;
             justify-content: center;
-            height: 20%;
+            height: 3rem;
             width: 80%;
 
             input {
                 background-color: rgb(232, 240, 254);
                 border: 2px solid #AAA;
-                height: 30%;
+                height: 1.5rem;
                 margin-left: 20px;
-            }
-
-            input:nth-child(3) {
-                height: 50%;
             }
 
             label {
@@ -89,6 +84,58 @@ const Container = styled.div`
 
         }
         
+    }
+
+    div#duas-div {
+        display: flex;
+        justify-content: space-around;
+        height: 20%;
+
+        div {
+            display: flex;
+            height: 100%;
+            width: 30%;
+            flex-direction: column;
+            align-items: center;
+
+            p {
+                margin-top: 2%;
+            }
+
+            p:nth-child(1) {
+                font-weight: bold;
+            }
+
+            p:nth-child(4) {
+                color: #003395;
+            }
+        }
+    }
+    
+    div#dois-p-footer {
+        align-items: center;
+        display: flex;
+        margin-top: 1%;
+        flex-direction: column;
+        justify-content: center;
+        height: 10%;
+        width: 100%;
+
+        p {
+            font-weight: bold;
+            margin-top: 10px;
+        }
+    }
+
+    div#footer-div {
+        align-items: center;
+        border-top: 3px solid #D99C44 ;
+        background-color: #404E82;
+        display: flex;
+        margin-top: 7.3%;
+        justify-content: center;
+        height: 5%;
+        width: 100%;
     }
 `;
 
